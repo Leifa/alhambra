45d6c96c8ecd990ccef79a3cc629dc7eb4c69ed5
leif <leif-s@bellek.de>
2020-01-08 15:13:40 +0100

    refactoring

---
+++ game.py
@@ -0,0 +1,92 @@
+import pygame
+from render import Renderer
+from reserve import Reserve
+from world import World
+from shop import Shop
+from stats import Stats
+import config
+
+
+class Game:
+
+    MODE_IDLE = 0
+    MODE_PLACE_CARD = 1
+
+    def __init__(self):
+        self.renderer = Renderer()
+        self.world = World(config.BOARD_WIDTH, config.BOARD_HEIGHT)
+        self.shop = Shop()
+        self.shop.randomize_cards()
+        self.reserve = Reserve()
+        self.stats = Stats()
+
+        self.card_in_placement = None
+        self.card_in_placement_cost = 0
+        self.mode = Game.MODE_IDLE
+        self.shop_slots = [pygame.rect.Rect(1040 + 140 * i, 100, config.TILESIZE, config.TILESIZE) for i in range(0, 4)]
+        self.reserve_slots = [pygame.rect.Rect(1040 + 140 * i, 385, config.TILESIZE, config.TILESIZE) for i in range(0, 4)]
+
+        self.running = False
+
+        pygame.display.init()
+        self.window = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
+
+    def run(self):
+        self.running = True
+        while self.running:
+            pygame.time.wait(30)
+
+            self.handle_events()
+
+            self.window.fill((0, 0, 0))
+            self.renderer.render(self.window, self, self.world, self.shop, self.reserve, self.stats)
+            pygame.display.update()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                x, y = event.pos
+                if event.button == pygame.BUTTON_RIGHT:
+                    self.stats.pride -= 2
+                    self.next_turn()
+                elif event.button == pygame.BUTTON_LEFT:
+                    if self.mode == Game.MODE_IDLE:
+                        # Shop Clicked
+                        for i in range(0, 4):
+                            if self.shop_slots[i].collidepoint(x, y):
+                                if self.stats.money >= self.shop.prices[i]:
+                                    self.card_in_placement = self.shop.cards[i].clone()
+                                    self.card_in_placement_cost = self.shop.prices[i]
+                                    self.mode = Game.MODE_PLACE_CARD
+                        # Reserve Clicked
+                        for i in range(0, 4):
+                            if self.reserve_slots[i].collidepoint(x, y):
+                                if self.reserve.cards[i] is not None:
+                                    self.card_in_placement = self.reserve.cards[i].clone()
+                                    self.reserve.cards[i] = None
+                                    self.card_in_placement_cost = 0
+                                    self.mode = Game.MODE_PLACE_CARD
+                    elif self.mode == Game.MODE_PLACE_CARD:
+                        bought_card = False
+                        for i in range(0, 4):
+                            if self.reserve_slots[i].collidepoint(x, y):
+                                self.reserve.cards[i] = self.card_in_placement
+                                bought_card = True
+                        x = x // config.TILESIZE
+                        y = y // config.TILESIZE
+                        if self.world.does_card_fit(self.card_in_placement, x, y):
+                            self.world.set_card(self.card_in_placement, x, y)
+                            bought_card = True
+                        if bought_card:
+                            self.mode = Game.MODE_IDLE
+                            self.stats.money -= self.card_in_placement_cost
+                            self.next_turn()
+
+    def next_turn(self):
+        self.shop.randomize_cards()
+        self.stats.money += 1
+        self.stats.money += self.world.get_number_of_arkaden()
+        self.stats.day += 1
+        self.mode = Game.MODE_IDLE
--- render.py
+++ render.py
@@ -1,7 +1,6 @@
 import pygame
 from card2 import Card
-
-TILESIZE = 56
+import config
 
 GREEN = (100, 200, 100)
 RED = (200, 100, 100)
@@ -12,15 +11,15 @@ class Renderer:
     def __init__(self):
         self.tiles = pygame.image.load('img/tiles54.png')
         self.tiles = pygame.transform.scale2x(self.tiles)
-        self.img_serail = self.load_image(0,0)
-        self.img_garden = self.load_image(1,0)
-        self.img_tower = self.load_image(2,0)
+        self.img_serail = self.load_image(0, 0)
+        self.img_garden = self.load_image(1, 0)
+        self.img_tower = self.load_image(2, 0)
         self.img_gemaecher = self.load_image(3, 0)
         self.img_pavillon = self.load_image(0, 1)
         self.img_arkaden = self.load_image(1, 1)
-        self.img_empty = self.load_image(2,1)
+        self.img_empty = self.load_image(2, 1)
 
-        self.img_wall_top = self.load_image(0,2)
+        self.img_wall_top = self.load_image(0, 2)
         self.img_wall_right = self.load_image(1, 2)
         self.img_wall_bottom = self.load_image(2, 2)
         self.img_wall_left = self.load_image(3, 2)
@@ -61,14 +60,14 @@ class Renderer:
             y = 0
             for card in row:
                 self.render_card(window, card, x, y)
-                y += 2*TILESIZE
-            x += 2*TILESIZE
-        window.blit(self.img_hud, (2*9*TILESIZE, 0))
+                y += config.TILESIZE
+            x += config.TILESIZE
+        window.blit(self.img_hud, (config.HUD_LEFT, 0))
 
         for i in range(0, 4):
             self.render_card(window, shop.cards[i], game.shop_slots[i].left, game.shop_slots[i].top)
             price = self.font.render(str(shop.prices[i]) + "€", True, GREEN if stats.money >= shop.prices[i] else RED)
-            window.blit(price, (game.shop_slots[i].left, game.shop_slots[i].top + TILESIZE + 60))
+            window.blit(price, (game.shop_slots[i].left, game.shop_slots[i].top + config.TILESIZE + 10))
 
         for i in range(0, 4):
             if reserve.cards[i] is not None:
@@ -90,6 +89,6 @@ class Renderer:
         window.blit(social, (1300, 690))
 
     def load_image(self, x, y):
-        img = pygame.Surface((TILESIZE * 2, TILESIZE * 2), pygame.SRCALPHA)
-        img.blit(self.tiles, (0, 0), (TILESIZE * 2 * x, TILESIZE * 2 * y, TILESIZE * 2, TILESIZE * 2))
+        img = pygame.Surface((config.TILESIZE, config.TILESIZE), pygame.SRCALPHA)
+        img.blit(self.tiles, (0, 0), (config.TILESIZE * x, config.TILESIZE * y, config.TILESIZE, config.TILESIZE))
         return img
